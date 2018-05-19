0d5f0521140f4983b4ca9992c88ce5f14e3409be
drivehappy <drivehappy@gmail.com>
2018-05-19 12:48:46 -0700

    Fixing incorrect marshaling of C# class. Classes are already references, therefore the 'ref' keyword is incorrectly applied here.

--- libzopfli-sharp/ZopfliCompressor.cs
+++ libzopfli-sharp/ZopfliCompressor.cs
@@ -24,7 +24,7 @@ namespace LibZopfliSharp.Native
         /// <param name="data_out">Pointer to the dynamic output array to which the result is appended</param>
         /// <param name="data_out_size">This is the size of the memory block pointed to by the dynamic output array size</param>
         [DllImport("x86\\zopfli.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern void ZopfliCompress(ref ZopfliOptions options, ZopfliFormat output_type, byte[] data, int data_size, ref IntPtr data_out, ref UIntPtr data_out_size);
+        public static extern void ZopfliCompress(ZopfliOptions options, ZopfliFormat output_type, byte[] data, int data_size, ref IntPtr data_out, ref UIntPtr data_out_size);
     }
 
     /// <summary>
@@ -42,6 +42,6 @@ namespace LibZopfliSharp.Native
         /// <param name="data_out">Pointer to the dynamic output array to which the result is appended</param>
         /// <param name="data_out_size">This is the size of the memory block pointed to by the dynamic output array size</param>
         [DllImport("amd64\\zopfli.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern void ZopfliCompress(ref ZopfliOptions options, ZopfliFormat output_type, byte[] data, int data_size, ref IntPtr data_out, ref UIntPtr data_out_size);
+        public static extern void ZopfliCompress(ZopfliOptions options, ZopfliFormat output_type, byte[] data, int data_size, ref IntPtr data_out, ref UIntPtr data_out_size);
     }
 }
